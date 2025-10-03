ae90b6ce07d6f36300d339f1a556ae52465a4158
Barry Horgan <horgan.barry@gmail.com>
2025-10-03 20:26:49 +1000

    fixed error responses

--- test/easywind.test.js
+++ test/easywind.test.js
@@ -33,16 +33,17 @@ describe('Easy Wind API', function () {
 });
 
     windStations.invalidStations.forEach((stationId) => {
-    it(`should return 404 response for invalid stations ${stationId}`, async () => {
+    it(`should return error for an invalid station ${stationId}`, async () => {
       const responsePromise = request(BASE_URL)
         .get(`${API_PREFIX}${stationId}/?period=latestdata/`);
-
-      await expect(responsePromise).to.eventually.have.property('status', 404);
+        
+      await expect(responsePromise).to.eventually.have.property('status', 200);
 
       const res = await responsePromise;
 
-      // Basic sanity check
       expect(res.body).to.be.an('object');
+      expect(res.body).to.have.property('error');
+      expect(res.body.error).to.match(/not implemented/i);
     });
   });
 
@@ -61,16 +62,17 @@ windStations.validPeriods.forEach((period) => {
 });
 
 windStations.invalidPeriods.forEach((period) => {
-    it(`should return 404 response for invalid periods ${period}`, async () => {
+    it(`should return error for invalid periods ${period}`, async () => {
       const responsePromise = request(BASE_URL)
         .get(`${API_PREFIX}EW013/?period=${period}/`);
 
-      await expect(responsePromise).to.eventually.have.property('status', 404);
+      await expect(responsePromise).to.eventually.have.property('status', 200);
 
       const res = await responsePromise;
 
-      // Basic sanity check
       expect(res.body).to.be.an('object');
+      expect(res.body).to.have.property('error');
+      expect(res.body.error).to.match(/not implemented/i);
     });
 });
 
