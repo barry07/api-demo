5b11616100c98a554066a0f7f185cb6594de0c2a
Barry Horgan <horgan.barry@gmail.com>
2025-10-03 21:27:39 +1000

    added bouy tests

--- test/2.getSocibWeatherStation.test.js
+++ test/2.getSocibWeatherStation.test.js
@@ -6,11 +6,43 @@ const expect = chai.expect;
 require("dotenv").config();
 
 const BASE_URL = process.env.BASE_URL;
-const API_PREFIX = "/v1.0/getEasyWind/";
+const API_PREFIX = "/v1.0/getSocibWeatherStation/";
 
 if (!BASE_URL) {
   throw new Error("BASE_URL not found. Ensure it is defined in your .env file.");
 }
 
 // Import station IDs from external file
-const windStations = require("../data/stations.json");
+const stations = require("../data/stations.json");
+
+describe('Get data from the socib bahia de palma buoy', function () {
+  this.timeout(10000); // give API time to respond
+
+  stations.validBouyStations.forEach((stationId) => {
+    it(`should return 200 response for valid bouy stations ${stationId}`, async () => {
+      const responsePromise = request(BASE_URL)
+        .get(`${API_PREFIX}${stationId}/latestdata/`);
+
+      await expect(responsePromise).to.eventually.have.property('status', 200);
+
+      const res = await responsePromise;
+
+      // Basic sanity check
+      expect(res.body).to.be.an('object');
+    });
+    });
+
+  stations.invalidStations.forEach((stationId) => {
+    it(`should return 404 response for invalid bouy stations ${stationId}`, async () => {
+      const responsePromise = request(BASE_URL)
+        .get(`${API_PREFIX}${stationId}/latestdata/`);
+
+      await expect(responsePromise).to.eventually.have.property('status', 404);
+
+      const res = await responsePromise;
+
+      // Basic sanity check
+      expect(res.body).to.be.an('object');
+    });
+    });
+});
